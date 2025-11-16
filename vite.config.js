e5a9b23b316bed6d7b8a691b79972ab958c1abfd
umutgorur511 <143970641+umutgorur511@users.noreply.github.com>
2025-11-16 17:06:14 +0300

    Fix React import and vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/loveyaso/', // GitHub Pages için repo adını yazdık
+  plugins: [react()],
+  base: '/loveyaso/'  // GitHub Pages için repo adı
 })
